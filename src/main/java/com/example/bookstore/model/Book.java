8359249870b107bf87019f7c7efbdd52695c0f9a
haksutin <jenna.hakala97@gmail.com>
2024-02-05 13:34:00 +0200

    first commit

---
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,51 @@
+package com.example.bookstore.model;
+
+public class Book {
+
+    private long id;
+    private String title;
+    private String authog;
+    private int publicationYear;
+    private String isbn;
+    private double price;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthog() {
+        return authog;
+    }
+    public void setAuthog(String authog) {
+        this.authog = authog;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
+
+
