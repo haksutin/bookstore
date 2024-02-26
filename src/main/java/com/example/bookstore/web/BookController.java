d110216ca6f3020237368b62fbdda1747e674d8d
haksutin <jenna.hakala97@gmail.com>
2024-02-26 17:32:16 +0200

    tehtävä tehty, virheet myös korjattu

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import java.util.List;
+import java.util.Optional;
+
 
 @Controller
 public class BookController {
@@ -38,6 +40,23 @@ public class BookController {
     @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable("id") Long id) {
         bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditBookForm(@PathVariable("id") long id, Model model) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            model.addAttribute("book", optionalBook.get());
+            return "editbook";
+        } else {
+            return "redirect:/booklist";
+        }
+    }
+
+    @PostMapping("/updateBook")
+    public String updateBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
         return "redirect:/booklist";
     }
 
